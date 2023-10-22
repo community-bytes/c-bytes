10007cee1a5aaabd709ccb6aad292e0f3c6bc01c
root <root@PATRYK-NFHTHTE0>
2023-10-21 23:39:44 -0400

    Finished getting posts from API

--- helpers/postsapi.py
+++ helpers/postsapi.py
@@ -5,45 +5,47 @@ import pandas as pd
 import re
 import pdb
 
-def extract_image_urls(cooked_text):
-    """Extract image URLs from the 'cooked' field."""
-    image_urls = re.findall(r'<img src="([^"]+)"', cooked_text)
-    return ", ".join(image_urls)
-
-def extract_file_urls(cooked_text):
-    """Extract attached file URLs from the 'cooked' field."""
-    file_urls = re.findall(r'<a class="attachment" href="([^"]+)"', cooked_text)
-    return ", ".join(file_urls)
-
-def parse_json_df(data):
-    """Parse JSON data to DataFrame and extract necessary fields and URLs."""
-    df = pd.json_normalize(data)
-    df = df[['id', 'name', 'username', 'cooked']].rename(columns={'cooked': 'text'})
-    df['image_urls'] = df['text'].apply(extract_image_urls)
-    df['file_urls'] = df['text'].apply(extract_file_urls)
-    return df
-
 def fetch_data(url, params):
     """Send a GET request and return the JSON data if successful."""
     response = requests.get(url, params=params)
     if response.status_code == 200:
         print("Request was successful!")
         return response.json()
-    else:
-        print("Request failed with status code", response.status_code)
-        print("Response Text:", response.text)
-        return None
+    print(f"Request failed with status code {response.status_code}")
+    print("Response Text:", response.text)
+    return None
 
-def save_to_csv(df, filename='output.csv'):
+def extract_image_urls(cooked_text):
+    """Extract image URLs from the 'cooked' field."""
+    return ", ".join(re.findall(r'<img src="([^"]+)"', cooked_text))
+
+def extract_file_urls(cooked_text):
+    """Extract attached file URLs from the 'cooked' field."""
+    return ", ".join(re.findall(r'<a class="attachment" href="([^"]+)"', cooked_text))
+
+def parse_json(data, is_solution=True):
+    """Parse JSON data to DataFrame and extract necessary fields and URLs."""
+    df = pd.json_normalize(data)
+    if is_solution:
+        columns = {
+            'id': 'answer-id',
+            'name': 'answer-name',
+            'username': 'answer-username',
+            'cooked': 'text'
+        }
+        df = df[['id', 'name', 'username', 'cooked']].rename(columns=columns)
+        df['image_urls'] = df['text'].apply(extract_image_urls)
+        df['file_urls'] = df['text'].apply(extract_file_urls)
+    else:
+        df = df[['id', 'cooked']].rename(columns={'id': 'og-id', 'cooked': 'og-text'})
+    return df
+
+def save_to_csv(df, filename='csv/output.csv'):
     """Save DataFrame to a CSV file."""
-    if os.path.exists(filename) and os.path.getsize(filename) > 0:
-        df.to_csv(filename, mode='a', header=False, index=False)
-    else:
-        df.to_csv(filename, mode='w', header=True, index=False)
+    mode = 'a' if os.path.exists(filename) and os.path.getsize(filename) > 0 else 'w'
+    header = mode == 'w'
+    df.to_csv(filename, mode=mode, header=header, index=False)
     print(f"Data has been appended to {filename}")
-
-import json
-import os
 
 def save_to_json(data, directory='jsons', filename='response.json'):
     """Save data to a JSON file."""
@@ -51,54 +53,60 @@ def save_to_json(data, directory='jsons', filename='response.json'):
         os.makedirs(directory)
     file_path = os.path.join(directory, filename)
     
-    if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
-        with open(file_path, 'r+') as f:
+    mode = 'r+' if os.path.exists(file_path) and os.path.getsize(file_path) > 0 else 'w'
+    with open(file_path, mode) as f:
+        if mode == 'r+':
             existing_data = json.load(f)
             if isinstance(existing_data, list) and isinstance(data, list):
                 existing_data.extend(data)
-                f.seek(0)
-                json.dump(existing_data, f)
-                f.truncate()
             elif isinstance(existing_data, dict) and isinstance(data, dict):
                 existing_data.update(data)
-                f.seek(0)
-                json.dump(existing_data, f)
-                f.truncate()
             else:
                 print("Error: Existing data and new data are not compatible for appending.")
                 return
-    else:
-        with open(file_path, 'w') as f:
+            f.seek(0)
+            json.dump(existing_data, f)
+            f.truncate()
+        else:
             json.dump(data, f)
     
     print(f"Response JSON has been appended to {file_path}")
 
-
 if __name__ == "__main__":
+    max_count = 80717 + 15
 
-    url_topics = f"https://discourse.mcneel.com/top.json"
-    params_topics = {"period": "all"}
-    response = fetch_data(url_topics, params_topics)
-    topic_list = response.get('topic_list')
-    topics = topic_list.get('topics')
-    topic_ids = []
-    for topic in topics:
-        topic_ids.append(topic.get('id'))
+    for i in range(80717 - 1500, max_count):
+        url = f"https://discourse.mcneel.com/t/{i}.json"
+        try:
+            response = fetch_data(url, params={})
+        except:
+            print (f"fetching of the id {i} failed")
+        
+        if not response or not response.get("accepted_answer"):
+            continue
+        
+        post_stream = response.get('post_stream', {})
+        data = post_stream.get('posts', [])
+        
+        if not data:
+            continue
+        
+        original_post = data[0]
+        topic_id = response.get("id")
+        
+        accepted_post_number = response.get("accepted_answer").get("post_number", 0)
+        accepted_answer = None
+        try:
 
-    max_count = 10
-    count = 0
-    for topic_id in topic_ids:
-        if count >= max_count:
-            print (count)
-            break
-        url = f"https://discourse.mcneel.com/t/{topic_id}.json"
-        params = {}
-        response = fetch_data(url, params)
-        post_stream = response.get('post_stream')
-        data = post_stream.get('posts')
+            accepted_answer = data[accepted_post_number - 1] if accepted_post_number > 0 else None
+        except:
+            print(f"Retrieving the accepted answer for the id {} failed")
+        
+        if accepted_answer:
+            df_solution = parse_json(accepted_answer)
+            df_original = parse_json(original_post, is_solution=False)
+            df_topic = pd.DataFrame({'topic-id': [topic_id]})
+            
+            result = pd.concat([df_solution, df_original, df_topic], axis=1)
+            save_to_csv(result)
 
-        if data:
-            df = parse_json_df(data)
-            save_to_csv(df)
-            save_to_json(data)
-        count += 1
